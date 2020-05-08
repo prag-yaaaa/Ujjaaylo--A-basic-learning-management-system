fcd6a2964f910b714392f1532b97d628555ddc76
prag-yaaaa <58254273+prag-yaaaa@users.noreply.github.com>
2020-05-08 09:13:28 +0545

    Update catagory.php

--- catagory.php
+++ catagory.php
@@ -1,44 +1,35 @@
 <!DOCTYPE html>
-<html lang="en">
+
 
 <head>
     <meta charset="UTF-8">
     <meta name="description" content="">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-
-    <!-- Title  -->
+    
+ 
     <title>Content Page</title>
 
-    <!-- Favicon  -->
     <link rel="icon" href="img/core-img/favicon.ico">
 
-    <!-- Style CSS -->
     <link rel="stylesheet" href="style.css">
 
 </head>
 <body>
 	
-    <!-- Preloader Start -->
     <div id="preloader">
         <div class="preload-content">
             <div id="world-load"></div>
         </div>
     </div>
-    <!-- Preloader End -->
 
-    <!-- ***** Header Area Start ***** -->
     <header class="header-area">
         <div class="container">
             <div class="row">
                 <div class="col-12">
                     <nav class="navbar navbar-expand-lg">
-                        <!-- Logo -->
                        <a class="navbar-brand" href="#"><b><h1 style="color:white;">उज्यालो</h1></b></a>
-                        <!-- Navbar Toggler -->
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#worldNav" aria-controls="worldNav" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                        <!-- Navbar -->
                         <div class="collapse navbar-collapse" id="worldNav">
                              <ul class="navbar-nav ml-auto">
                                 
@@ -53,7 +44,6 @@
                                 </li>
                             </ul>
                             </ul>
-                            <!-- Search Form  -->
                             <div id="search-wrapper">
                                 <form action="#">
                                     <input type="text" id="search" placeholder="Search something...">
@@ -67,19 +57,14 @@
             </div>
         </div>
     </header>
-    <!-- ***** Header Area End ***** -->
 
-    <!-- ********** Hero Area Start ********** -->
     <div class="hero-area height-400 bg-img background-overlay" style="background-image: url(img/blog-img/bg.jpg);"></div>
-    <!-- ********** Hero Area End ********** -->
 
     <div class="main-content-wrapper section-padding-100">
         <div class="container">
             <div class="row justify-content-center">
-                <!-- ============= Post Content Area Start ============= -->
                 <div class="col-12 col-lg-8">
                     <div class="post-content-area mb-100">
-                        <!-- Catagory Area -->
                         <div class="world-catagory-area">
                             <ul class="nav nav-tabs" id="myTab" role="tablist">
                                 <li class="title">Subjects included</li>
@@ -93,13 +78,10 @@
                             <div class="tab-content" id="myTabContent">
 
                                 <div class="tab-pane fade show active" id="world-tab-1" role="tabpanel" aria-labelledby="tab1">
-                                    <!-- Single Blog Post -->
                                     <div class="single-blog-post post-style-4 d-flex align-items-center">
-                                        <!-- Post Thumbnail -->
                                         <div class="post-thumbnail">
                                             <img src="img/blog-img/science.jpg" alt="">
                                         </div>
-                                        <!-- Post Content -->
                                         <div class="post-content">
                                             <a href="#" class="headline">
                                                 <h5>Science?</h5>
@@ -109,13 +91,10 @@
                                         </div>
                                     </div>
 
-                                    <!-- Single Blog Post -->
                                     <div class="single-blog-post post-style-4 d-flex align-items-center">
-                                        <!-- Post Thumbnail -->
                                         <div class="post-thumbnail">
                                             <img src="img/blog-img/math.jpg" alt="">
                                         </div>
-                                        <!-- Post Content -->
                                         <div class="post-content">
                                             <a href="#" class="headline">
                                                 <h5>Math?</h5>
@@ -125,13 +104,10 @@
                                         </div>
                                     </div>
 
-                                    <!-- Single Blog Post -->
                                     <div class="single-blog-post post-style-4 d-flex align-items-center">
-                                        <!-- Post Thumbnail -->
                                         <div class="post-thumbnail">
                                             <img src="img/blog-img/english.jpg" alt="">
                                         </div>
-                                        <!-- Post Content -->
                                         <div class="post-content">
                                             <a href="#" class="headline">
                                                 <h5>English?</h5>
@@ -141,13 +117,10 @@
                                         </div>
                                     </div>
 
-                                    <!-- Single Blog Post -->
                                     <div class="single-blog-post post-style-4 d-flex align-items-center">
-                                        <!-- Post Thumbnail -->
                                         <div class="post-thumbnail">
                                             <img src="img/blog-img/account.jpg" alt="">
                                         </div>
-                                        <!-- Post Content -->
                                         <div class="post-content">
                                             <a href="#" class="headline">
                                                 <h5>Accounts?</h5>
@@ -157,13 +130,10 @@
                                         </div>
                                     </div>
 
-                                    <!-- Single Blog Post -->
                                     <div class="single-blog-post post-style-4 d-flex align-items-center">
-                                        <!-- Post Thumbnail -->
                                         <div class="post-thumbnail">
                                             <img src="img/blog-img/gk.png" alt="">
                                         </div>
-                                        <!-- Post Content -->
                                         <div class="post-content">
                                             <a href="#" class="headline">
                                                 <h5>General Knowledge?</h5>
@@ -173,13 +143,10 @@
                                         </div>
                                     </div>
 
-                                    <!-- Single Blog Post -->
                                     <div class="single-blog-post post-style-4 d-flex align-items-center">
-                                        <!-- Post Thumbnail -->
                                         <div class="post-thumbnail">
                                             <img src="img/blog-img/cs.jpg" alt="">
                                         </div>
-                                        <!-- Post Content -->
                                         <div class="post-content">
                                             <a href="#" class="headline">
                                                 <h5>Computer Science?</h5>
@@ -194,12 +161,10 @@
                     </div>
                 </div>
 
-                <!-- ========== Sidebar Area ========== -->
                 <div class="col-12 col-md-8 col-lg-4">
                     <div class="post-sidebar-area">
                        
                         
-                        <!-- Widget Area -->
                         <div class="sidebar-widget-area">
                             <h5 class="title">Stay Connected</h5>
                             <div class="widget-content">
@@ -213,24 +178,19 @@
                                 </div>
                             </div>
                         </div>
-                        <!-- Widget Area -->
                         <div class="sidebar-widget-area">
                             <h5 class="title">Today’s Pick</h5>
                             <div class="widget-content">
-                                <!-- Single Blog Post -->
                                 <div class="single-blog-post todays-pick">
-                                    <!-- Post Thumbnail -->
                                     <div class="post-thumbnail">
                                         <img src="img/blog-img/bg.jpg" alt="">
                                     </div>
-                                    <!-- Post Content -->
                                     <div class="post-content px-0 pb-0">
                                         <a href="https://www.forbes.com/forbes/welcome/?toURL=https://www.forbes.com/education/&refURL=https://www.google.com.np/&referrer=https://www.google.com.np/" class="headline">
                                             <h5>Forbes Education</h5>
                                         </a>
                                     </div>
                                 </div>
-								<!-- Load More btn -->
 								<div class="row">
 									<div class="col-12">
 										<div class="load-more-btn mt-50 text-center">
@@ -248,7 +208,6 @@
         </div>
     </div>
 
-    <!-- ***** Footer Area Start ***** -->
     <footer class="footer-area">
         <div class="container">
             <div class="row">
@@ -256,9 +215,9 @@
                     <div class="footer-single-widget">
                         <a class="navbar-brand" href="index.html"><b><h1 style="color:white;">उज्यालो</h1></b></a>
                         <div class="copywrite-text mt-30">
-                            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                            <p>
 Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This site is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Pragya</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+</p>
                         </div>
                     </div>
                 </div>
@@ -284,18 +243,6 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
             </div>
         </div>
     </footer>
-    <!-- ***** Footer Area End ***** -->
-
-    <!-- jQuery (Necessary for All JavaScript Plugins) -->
-    <script src="js/jquery/jquery-2.2.4.min.js"></script>
-    <!-- Popper js -->
-    <script src="js/popper.min.js"></script>
-    <!-- Bootstrap js -->
-    <script src="js/bootstrap.min.js"></script>
-    <!-- Plugins js -->
-    <script src="js/plugins.js"></script>
-    <!-- Active js -->
-    <script src="js/active.js"></script>
 
 </body>
 

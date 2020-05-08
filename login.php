a4163899b4896e0e8e708a1724ff05b6a4bf83f9
prag-yaaaa <58254273+prag-yaaaa@users.noreply.github.com>
2020-05-08 09:26:43 +0545

    Update login.php

--- login.php
+++ login.php
@@ -1,46 +1,35 @@
 
 <!DOCTYPE html>
-<html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="description" content="">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
-    <!-- Title  -->
     <title>Login page</title>
 
-    <!-- Favicon  -->
     <link rel="icon" href="img/core-img/favicon.ico">
 
-    <!-- Style CSS -->
     <link rel="stylesheet" href="style.css">
 
 </head>
 
 <body>
 	
-    <!-- Preloader Start -->
     <div id="preloader">
         <div class="preload-content">
             <div id="world-load"></div>
         </div>
     </div>
-    <!-- Preloader End -->
 
-    <!-- ***** Header Area Start ***** -->
     <header class="header-area">
         <div class="container">
             <div class="row">
                 <div class="col-12">
                     <nav class="navbar navbar-expand-lg">
-                        <!-- Logo -->
                         <a class="navbar-brand" href="index.php"><b><h1 style="color:white;">उज्यालो</h1></b></a>
-                        <!-- Navbar Toggler -->
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#worldNav" aria-controls="worldNav" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                        <!-- Navbar -->
                         <div class="collapse navbar-collapse" id="worldNav">
                             <ul class="navbar-nav ml-auto">
                                 <li class="nav-item active">
@@ -64,7 +53,6 @@
                                     <a class="nav-link" href="contact.php">Contact</a>
                                 </li>
                             </ul>
-                            <!-- Search Form  -->
                             <div id="search-wrapper">
                                 <form action="#">
                                     <input type="text" id="search" placeholder="Search something...">
@@ -78,11 +66,8 @@
             </div>
         </div>
     </header>
-    <!-- ***** Header Area End ***** -->
 
-    <!-- ********** Hero Area Start ********** -->
     <div class="hero-area height-400 bg-img background-overlay" style="background-image: url(img/blog-img/bg.jpg);"></div>
-    <!-- ********** Hero Area End ********** -->
 
     <div class="regular-page-wrap section-padding-100">
         <div class="container">
@@ -177,7 +162,6 @@
         </div>
     </div>
 
-    <!-- ***** Footer Area Start ***** -->
     <footer class="footer-area">
         <div class="container">
             <div class="row">
@@ -185,9 +169,9 @@
                     <div class="footer-single-widget">
                          <a class="navbar-brand" href="index.html"><b><h1 style="color:white;">उज्यालो</h1></b></a>
                         <div class="copywrite-text mt-30">
-                            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                            <p>
 Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This site is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Pragya</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+</p>
                         </div>
                     </div>
                 </div>
@@ -213,19 +197,7 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
                 </div>
             </div>
         </div>
-    </footer>
-    <!-- ***** Footer Area End ***** -->
-
-    <!-- jQuery (Necessary for All JavaScript Plugins) -->
-    <script src="js/jquery/jquery-2.2.4.min.js"></script>
-    <!-- Popper js -->
-    <script src="js/popper.min.js"></script>
-    <!-- Bootstrap js -->
-    <script src="js/bootstrap.min.js"></script>
-    <!-- Plugins js -->
-    <script src="js/plugins.js"></script>
-    <!-- Active js -->
-    <script src="js/active.js"></script>
+	</footer>
 
 </body>
 
